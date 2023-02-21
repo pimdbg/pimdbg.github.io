64083dbcf3f9ea6eaab0b24e6ebfd704c085b1ad
pimdbg <84320205+pimdbg@users.noreply.github.com>
2023-02-21 18:07:38 +0100

    Update Hexagon

--- src/components/Hexagon.js
+++ src/components/Hexagon.js
@@ -1,29 +1,28 @@
-import { useEffect } from 'react';
 import '../hexagon.css';
 import Observer from './Observer';
 
 function Hexagon(props) {
-    const { size = 100, icon , className } = props;
+    const { icon , className } = props;
 
     return (
-        <div className={"hexagon " + className}>
-            {/* functions as border */}
-            <div className="hexagon__top" />
-            <div className="hexagon__middle" />
-            <div className="hexagon__bottom" />
         <Observer>
+            <div className={"hexagon " + className}>
+                {/* functions as border */}
+                <div className="hexagon__top" />
+                <div className="hexagon__middle" />
+                <div className="hexagon__bottom" />
 
-            {/* Inner content */}
-            <div className="hexagon hexagon-inner">
-                <div className="hexagon__top hexagon-inner__top" />
-                <div className="hexagon__middle hexagon-inner__middle">
-                    <div className="hexagon__content">
-                        {icon}
+                {/* Inner content */}
+                <div className="hexagon hexagon-inner">
+                    <div className="hexagon__top hexagon-inner__top" />
+                    <div className="hexagon__middle hexagon-inner__middle">
+                        <div className="hexagon__content">
+                            {icon}
+                        </div>
                     </div>
+                    <div className="hexagon__bottom hexagon-inner__bottom" />
                 </div>
-                <div className="hexagon__bottom hexagon-inner__bottom" />
             </div>
-        </div>
         </Observer>
     );
 }
