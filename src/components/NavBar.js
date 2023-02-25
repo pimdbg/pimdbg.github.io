c429042a1f11415c3e07129bf70c92e4fd29a3c3
pimdbg <84320205+pimdbg@users.noreply.github.com>
2023-02-25 20:03:13 +0100

    Update NavBar component, add mobile navbar as well

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,70 @@
 function NavBar() {
+import { useState } from 'react';
+import { SlMenu, SlHome } from 'react-icons/sl';
+import Socials from '../components/Socials';
+
+    const [links, setLinks] = useState([
+        {name: "Home", href: "/", icon: <SlHome color={"white"} />},
+        {name: "About", href: "/#about", icon: <SlHome color={"white"} />},
+        {name: "Projects", href: "/#projects"}
+    ]);
+
     return (
-        <nav className="nav-bar">
-            <ul className="nav-menu layout">
-                <li className="nav-menu__item"><a href="#">Home</a></li>
-                <li className="nav-menu__item"><a href="#">About</a></li>
-            </ul>
-        </nav>
+        <div className="nav-bar">
+            <div className="nav-wrapper">
+                <nav className="nav">
+                    <ul className="nav__menu nav-menu layout">
+                        {
+                            links.map((link, index) => (
+                                <li key={index} className="nav-menu__item">
+                                    <a href={link.href}>{link.name}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </nav>
+            </div>
+            <NavMobile />
+        </div>
     );
+
+    function NavMobile() {
+        const [isOpened, setIsOpened] = useState(false);
+        let transitionDuration = 300;
+
+        return (
+            <div className="nav-mobile-wrapper layout">
+                
+                {/* Menu button */}
+                <button className="nav-mobile__btn" onClick={() => setIsOpened(! isOpened)}>
+                    <SlMenu color={"white"} size={25} />
+                </button>
+                
+                <nav className={"nav-mobile " + ((isOpened) ? "nav-mobile--opened" : "")} 
+                    style={{ 
+                        translate: (isOpened) ? 0 : "100vmax",
+                        transition: `translate ${transitionDuration}ms ease-out` 
+                    }}>
+                    <ul className="nav-mobile__menu nav-menu layout">
+                        {
+                            links.map((link, index) => (
+                                <li key={index} 
+                                    className="nav-menu__item" 
+                                    style={{ 
+                                        translate: (isOpened) ? 0 : "100vmax",
+                                        filter: `blur(${(isOpened) ? "0px" : "4px"})`,  
+                                        transition: (isOpened) && `all 500ms ${ (transitionDuration + (index * 200)) }ms`,
+                                    }}
+                                >
+                                    <a href={link.href} onClick={() => setIsOpened(false)}>{link.name}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
 }
 
 export default NavBar;
